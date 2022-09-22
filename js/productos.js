541c3c876dd69ea485062ba743348355f3d0d123
German Gauna <germangauna88@gmail.com>
2022-09-21 23:08:03 -0300

    se agrego archivo productos de javascript

---
+++ js/productos.js
@@ -0,0 +1,51 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "paisaje",
+        desc: "grafica",
+        precio: 5000,
+        img: "./multimedia/pintura1.jpg",
+        cantidad: 1
+    },
+    {
+        id: 2,
+        nombre: "Mona Lisa",
+        desc: "grafica",
+        precio: 15000,
+        img: "./multimedia/pintura2.jpg",
+        cantidad: 1
+    },
+    {
+        id: 3,
+        nombre: "Bailarina",
+        desc: "grafica",
+        precio: 10000,
+        img: "./multimedia/pintura3.jpg",
+        cantidad: 1
+    },
+    {
+        id: 4,
+        nombre: "Grito",
+        desc: "grafica",
+        precio: 14000,
+        img: "./multimedia/pintura4.jpg",
+        cantidad: 1
+    },
+    {
+        id: 5,
+        nombre: "Colibri",
+        desc: "grafica",
+        precio: 9000,
+        img: "./multimedia/pintura5.jpg",
+        cantidad: 1
+    },
+    {
+        id: 6,
+        nombre: "Paris",
+        desc: "grafica",
+        precio: 8000,
+        img: "./multimedia/pintura6.jpg",
+        cantidad: 1
+    },
+
+];
